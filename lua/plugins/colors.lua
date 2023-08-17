da6408d42b83603338065ac6283b74a62ba08e3d
Hugh Ackland <hugh.ackland@eliiza.com.au>
2023-08-18 09:22:43 +1000

    Change line numbering colours. A bit naughty.

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,11 +1,15 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme tokyonight]])
-    end,
-  },
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+            -- load the colorscheme here
+            vim.cmd([[colorscheme tokyonight]])
+            vim.cmd([[highlight Comment guifg=#7799cc]])
+            vim.cmd([[highlight LineNr guifg=#eeeeee]])
+            vim.cmd([[highlight LineNrAbove guifg=#aaccff]])
+            vim.cmd([[highlight LineNrBelow guifg=#aaccff]])
+        end,
+    },
 }
